f59b739b3985bf61eac447db229a0e72db08f1de
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-11-14 23:58:49 +0100

    fix

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,8 +1,9 @@
 import UserCard from "@/components/common/UserCard";
 import Header from "@/components/layout/Header";
 import { UserProps } from "@/interfaces";
+import { PostProps } from '../../interfaces/index';
 
-const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
+const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
@@ -14,7 +15,7 @@ const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
           </button>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {users?.map((user: UserProps) => (
+          {posts?.map((user: UserProps) => (
             <UserCard 
               key={user.id}
               id={user.id}
