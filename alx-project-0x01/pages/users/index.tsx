3a69553a063b5a7c331fb6ff819be90d447a6e5a
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-11-14 15:53:14 +0100

    Header Setup

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Header from "@/components/layout/Header";
 
 function index() {
   return (
-    <div>index</div>
+    <div>
+      <Header />
+    </div>
   )
 }
 
