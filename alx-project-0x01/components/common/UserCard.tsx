77ec441ee4b6f81634373954a2100c1bf6711e11
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-11-14 23:45:11 +0100

    User Card

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,29 @@
+import {  UserProps } from "@/interfaces";
+
+const UserCard: React.FC<UserProps> = ({name, username, email, address, phone, website, company  }) => {
+  return (
+  <div className="max-w-sm mx-auto my-4 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 border border-gray-200">
+      <div className="mb-4">
+        <h2 className="text-xl font-semibold text-gray-800">{name}</h2>
+        <p className="text-sm  text-gray-500">@{username}</p>
+      
+      </div>
+      
+      <div className="space-y-2 text-sm text-gray-600">
+        <p className="text-gray-600 text-sm">Company: {company.name}</p>
+        <p>Email:  {email}</p>
+        <p>Phone:  {phone}</p>
+        <p> Website: {website}</p>
+        <p>Motto: {company.catchPhrase}</p>
+        <p>Address: {address.city}, {address.street}</p>
+      </div>
+      
+      <div className="mt-4 pt-4 border-t border-gray-200">
+        <p className="text-xs text-gray-500">Business: {company.bs}</p>
+      </div>
+    </div>
+
+  );
+};
+
+export default UserCard;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import UserCard from "@/components/common/UserCard";
 import Header from "@/components/layout/Header";
+import { UserProps } from "@/interfaces";
 
-function index() {
+const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
+      <main className="p-4 flex-grow">
+        <div className="flex justify-between items-center mb-6">
+          <h1 className="text-2xl font-semibold">Users</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white hover:bg-blue-800 transition">
+            Add User
+          </button>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {users?.map((user: UserProps) => (
+            <UserCard 
+              key={user.id}
+              id={user.id}
+              name={user.name}
+              username={user.username}
+              email={user.email}
+              address={user.address}
+              phone={user.phone}
+              website={user.website}
+              company={user.company}
+            />
+          ))}
+        </div>
+      </main>
     </div>
-  )
+     );
 }
 
-export default index
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const users = await response.json()
+
+  return {
+    props: {
+      users
+    }
+  }
+}
+
+export default Users;
